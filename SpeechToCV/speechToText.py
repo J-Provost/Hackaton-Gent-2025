4c3f580f2fd394df352a3fe9d67e9b8010b32363
Funtime <182619636+DavidTangievUCLL@users.noreply.github.com>
2025-04-26 02:05:04 +0200

    Logging  STT enabled

--- SpeechToCV/speechToText.py
+++ SpeechToCV/speechToText.py
@@ -2,7 +2,8 @@ import assemblyai as aai
 import json
 
 
-def speech_to_text(audio_file) -> str:
+def speech_to_text(audio_file: str) -> str:
+    "Neemt een string van audio path file en returned Text van de audio file"
     with open("Hackaton-Gent-2025/SpeechToCV/API.json", "r") as api_file:
         file = json.load(api_file)
         API_KEY = file["assemblyAI"]
@@ -16,4 +17,8 @@ def speech_to_text(audio_file) -> str:
         raise RuntimeError(f"Transcription failed: {transcript.error}")
     return transcript.text
 
-print(speech_to_text("Hackaton-Gent-2025/SpeechToCV/Silence Wench.mp3"))
+text_transcript = speech_to_text("Hackaton-Gent-2025/SpeechToCV/Silence Wench.mp3")
+
+print(text_transcript)
+with open("Hackaton-Gent-2025/rapporten/text_transcript1", "w") as file:
+    file.write(text_transcript)
